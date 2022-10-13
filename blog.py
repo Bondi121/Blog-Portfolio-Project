9fbe57319150e0833bd3e16e1819aa8bb378c7ce
Gabriel <gabriel.guerra@hotmail.de>
2022-10-13 16:09:52 -0500

    Adding logic to registration endpoint for creating a user

--- blog.py
+++ blog.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import os 
 
 app = Flask(__name__)
+db = SQLAlchemy()
+basedir = os.path.abspath(os.path.dirname('1800 Final Project Blog Post'))
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'blog.sqlite')
+print(basedir)
+
+db.init_app(app)
+
+class User(db.Model):
+    #Creating user table schema
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    address = db.Column(db.String, unique=True, nullable=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+
+
+#Creating tables        
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/homepage')
 def home():
@@ -10,11 +33,21 @@ def home():
 
 @app.route('/registration', methods=['POST'])
 def register():
+    user_data = request.json
+    firstname = user_data.get('firstname')
+    lastname = user_data.get('lastname')
+    username = user_data.get('username')
+    email = user_data.get('email')
+    if request.method == 'POST':
+        user = User(first_name = firstname, last_name = lastname, username = username, email = email)
+        db.session.add(user) 
+        db.session.commit()
+    print(user_data)
     return "Registration"
 
 
-@app.route('/profile')
-def user_profile():
+@app.route('/profile/<id>', methods=['GET'])
+def user_profile(id):
     return "User Profile"
 
 
@@ -50,6 +83,9 @@ def update_post():
 
 
 
+#python -m venv venv
+#venv\Scripts\activate
+#pip install Flask
 
 #set FLASK_APP=blog.py
 #set FLASK_DEBUG=True
@@ -66,3 +102,4 @@ def update_post():
 #Pagination
 
 #Local Storage
+
